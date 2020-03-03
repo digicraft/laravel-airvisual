5c78b99319b3c26e07c39455f89b3d7fc3c83965
Elias Peraza <edp.matheus@gmail.com>
2020-03-03 19:28:18 -0400

    Bug fixed

--- src/AirVisualServiceProvider.php
+++ src/AirVisualServiceProvider.php
@@ -13,7 +13,7 @@ class AirVisualServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/airvisual.php', 'laralang');
+        $this->mergeConfigFrom(__DIR__.'/../config/airvisual.php', 'airvisual');
     }
 
     /**
